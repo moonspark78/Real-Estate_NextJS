abc11d04b048de689d7cdbfb30155e7c39e3b9c4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-11 01:49:07 +0200

    :tada: starting with Navbar  :tada:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Roboto} from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const font =  Roboto({
     subsets: ["latin"],
@@ -21,7 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={font.className}>
-          {children}
+          <ResponsiveNav/>
+            {children}
       </body>
     </html>
   );
