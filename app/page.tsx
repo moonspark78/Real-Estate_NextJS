394a9eec3b6728bd8b6e3b8b25fc3aa8770944f3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-10 19:43:40 +0200

    :tada: adding data  :tada:

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Home from '@/components/Home/Home'
 import React from 'react'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return( 
+    <div>
+      <Home/>
+    </div>
+  )
 }
 
 export default HomePage
