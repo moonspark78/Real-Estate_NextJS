1e4e1698d5c4628cefcfd2d0ba19e0e67d2609cc
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-12 15:02:42 +0200

    :tada: create the Hero Section  :tada:

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative bg-[url('/images/hero.jpg')] bg-cover bg-center">
+        Hero
+    </div>
+  )
+}
+
+export default Hero
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -33,7 +33,7 @@ const Nav = ({openNav} : Props) => {
 
 
   return (
-    <div className={`fixed ${navBg ? "bg-gray-800" : ""} h-[10vh] z-[100] w-full transition-all duration-200 bg-gray-700`}>
+    <div className={`fixed ${navBg ? "bg-gray-800" : ""} h-[10vh] z-[100] w-full transition-all duration-200`}>
         <div className='flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto '>
 
 
