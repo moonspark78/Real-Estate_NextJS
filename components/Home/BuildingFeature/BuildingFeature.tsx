23593dc8ada32fef8926d6965f698c858d8b89c7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-16 16:12:38 +0200

    :wrench: done  BuildingFeatureCard Component   :wrench:

--- components/Home/BuildingFeature/BuildingFeature.tsx
+++ components/Home/BuildingFeature/BuildingFeature.tsx
@@ -8,7 +8,7 @@ const BuildingFeature = () => {
     <div className='pb-16 pt-16 bg-gray-100'>
         <div className='w-[80%] mx-auto'>
             <SectionHeading heading='Explor Building Feature'/>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 items-center mt-14'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center mt-14'>
                 {
                     buildings.map((building) => {
                         return (
--- components/Home/BuildingFeature/BuildingFeatureCard.tsx
+++ components/Home/BuildingFeature/BuildingFeatureCard.tsx
@@ -1,4 +1,7 @@
+"use client";
+import Image from 'next/image';
 import React from 'react'
+import Tilt from 'react-parallax-tilt';
 
 
 type Props ={
@@ -10,9 +13,23 @@ type Props ={
     }
 }
 
-const BuildingFeatureCard = ({}: Props) => {
+const BuildingFeatureCard = ({building}: Props) => {
   return (
-    <div>BuildingFeatureCard</div>
+    <Tilt className='rounded-lg p-4 shadow-md bg-white'>
+        <div className='w-16 h-16 ml-auto flex items-center justify-center flex-col bg-gray-100 rounded-full'>
+            <Image
+                src={building.image}
+                alt={building.title}
+                width={40}
+                height={40}
+            />
+        </div>
+        <div className='p-2'>
+            <p className='mt-6 text-red-600 font-bold text-xl'>{building.id}</p>
+            <p className='text-lg text-gray-700 font-bold mt-2 mb-2'>{building.title}</p>
+            <p className='text-sm opacity-70 text-gray-700'>{building.description}</p>
+        </div>
+    </Tilt>
   )
 }
 
