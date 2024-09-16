11c585599253d7831dd2eb6184a4e799b4043efe
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-16 14:01:44 +0200

    :wrench: done cityCard   :wrench:

--- components/Home/CityProperties/CityCard.tsx
+++ components/Home/CityProperties/CityCard.tsx
@@ -25,6 +25,13 @@ const CityCard = ({city}: Props) => {
             height={700}
             className='rounded-lg w-full h-[450px] object-cover'
         />
+
+        <div className='absolute inset-0 bg-black opacity-40'></div>
+        <div className='absolute p-4 top-4'>
+            <h1 className='text-lg font-semibold text-white'>{city.cityName}</h1>
+            <p className='text-gray-200'>{city.numberOfProperties} Properties</p>
+        </div>
+
     </div>
   )
 }
