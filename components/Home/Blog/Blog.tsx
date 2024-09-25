cd34d1d6f1a21e6f1693b146a527f7af20d911df
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-25 21:16:52 +0200

    new compenent Blog  :art:

---
+++ components/Home/Blog/Blog.tsx
@@ -0,0 +1,15 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div className='pt-16 pb-16 bg-gray-100'>
+        <div className='w-[80%] mx-auto'>
+            <SectionHeading heading='Check Our Blogs'/>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Blog
